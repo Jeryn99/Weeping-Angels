3c99755b32fb44545f182fbc54f69e70af2afd13
Sub <craigtbalmer@outlook.com>
2018-03-31 22:38:43 +0000

    Version bump

--- src/main/java/com/github/reallysub/angels/WeepingAngels.java
+++ src/main/java/com/github/reallysub/angels/WeepingAngels.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class WeepingAngels {
 	public static final String MODID = "weeping-angels";
 	public static final String NAME = "Weeping Angels";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "3.0";
 	
 	private static Logger logger;
 	
