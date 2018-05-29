d1932e9f00d29b71c15b8ae4c6a46db4eda7b3fe
Sea <craigtbalmer@outlook.com>
2018-05-29 22:32:46 +0100

    Update json

--- src/main/java/me/sub/angels/main/WeepingAngels.java
+++ src/main/java/me/sub/angels/main/WeepingAngels.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class WeepingAngels {
 	public static final String MODID = "weeping-angels";
 	public static final String NAME = "Weeping Angels";
-	public static final String VERSION = "10.5";
+	public static final String VERSION = "11";
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.2.2638,)";
 	
 	@EventHandler
