c248f464be256bc5bcbda8bdef72cf43aca9b618
Sub <craigtbalmer@outlook.com>
2018-06-20 18:45:24 +0100

    Josn update thingo

--- src/main/java/me/sub/angels/main/WeepingAngels.java
+++ src/main/java/me/sub/angels/main/WeepingAngels.java
@@ -21,8 +21,8 @@ public class WeepingAngels {
 
 	public static final String MODID = "weeping-angels";
 	public static final String NAME = "Weeping Angels";
-	public static final String VERSION = "14";
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.2.2638,)";
+	public static final String VERSION = "14.5";
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.4.2706,)";
 	
 	@EventHandler
 	public void pre(FMLPreInitializationEvent event) {}
