3ed0c9795beece34f165db97d68e5e92778699b6
Sub <craigtbalmer@outlook.com>
2018-06-23 21:37:01 +0100

    HOP HOP

--- src/main/java/me/sub/angels/main/WeepingAngels.java
+++ src/main/java/me/sub/angels/main/WeepingAngels.java
@@ -21,7 +21,7 @@ public class WeepingAngels {
 
 	public static final String MODID = "weeping-angels";
 	public static final String NAME = "Weeping Angels";
-	public static final String VERSION = "14.5";
+	public static final String VERSION = "15";
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.4.2706,)";
 	
 	@EventHandler
