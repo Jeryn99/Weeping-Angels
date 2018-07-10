e3b81c62e473cfcb76f13f16b84520462e2f616b
Sub <craigtbalmer@outlook.com>
2018-07-10 19:00:18 +0100

    Re-added: Update checker & Snow arm generation

--- src/main/java/me/sub/angels/WeepingAngels.java
+++ src/main/java/me/sub/angels/WeepingAngels.java
@@ -8,13 +8,14 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = WeepingAngels.MODID, name = WeepingAngels.NAME, version = WeepingAngels.VERSION)
+@Mod(modid = WeepingAngels.MODID, name = WeepingAngels.NAME, version = WeepingAngels.VERSION, dependencies = WeepingAngels.DEPENDENCIES, updateJSON = WeepingAngels.VERSION_CHECK)
 public class WeepingAngels {
 
     public static final String MODID = "weeping-angels";
     public static final String NAME = "Weeping Angels";
     public static final String VERSION = "17";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.4.2706,)";
+    public static final String VERSION_CHECK = "https://raw.githubusercontent.com/ReallySub/Weeping-Angels-Mod/master/update.json";
 
 
     @Mod.Instance(MODID)
--- src/main/java/me/sub/angels/common/events/EventHandler.java
+++ src/main/java/me/sub/angels/common/events/EventHandler.java
@@ -6,6 +6,7 @@ import me.sub.angels.common.events.mods.EventAngelTeleport;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
@@ -13,10 +14,20 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
@@ -31,6 +42,48 @@ public class EventHandler {
             player.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 600, 3));
         }
     }
+
+    /**
+     * Update checker thing, tells the player that the mods out of date if they're on a old build
+     */
+    @SubscribeEvent
+    public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent e) {
+        EntityPlayer player = e.player;
+        if (!player.world.isRemote) {
+            ForgeVersion.CheckResult version = ForgeVersion.getResult(Loader.instance().activeModContainer());
+            if (version.status == ForgeVersion.Status.OUTDATED) {
+                TextComponentString url = new TextComponentString(TextFormatting.AQUA + TextFormatting.BOLD.toString() + "UPDATE");
+                url.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.curseforge.com/projects/weeping-angels-mod"));
+                url.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString("Open URL")));
+
+                player.sendMessage(new TextComponentString(TextFormatting.GOLD + "[Weeping Angels] : ").appendSibling(url));
+                String changes = String.valueOf(version.changes).replace("{" + version.target + "=", "").replace("}", "");
+                player.sendMessage(new TextComponentString(TextFormatting.GOLD + "Changes: " + TextFormatting.BLUE + changes));
+            }
+        }
+    }
+
+    /*
+     * Spawning arms in snow biomes
+     */
+    @SubscribeEvent
+    public static void decorateBiomeEvent(DecorateBiomeEvent e) {
+        if (e.getWorld().getBiome(e.getPos()).isSnowyBiome()) {
+            if (e.getRand().nextInt(30) <= 10) {
+                generate(e.getWorld(), e.getPos());
+            }
+        }
+    }
+
+    private static void generate(World world, BlockPos position) {
+        BlockPos pos = new BlockPos(position.add(new BlockPos(8, 0, 8)));
+        if ((!world.provider.isNether() || pos.getY() < 255) && world.getBiome(position).isSnowyBiome()) {
+            if (world.getBlockState(pos).getBlock() == Blocks.SNOW || world.getBlockState(pos).getBlock() == Blocks.SNOW_LAYER)
+                world.setBlockState(pos, WAObjects.Blocks.ARM.getDefaultState(), 1);
+        }
+    }
+
+
 
     @SubscribeEvent
     public static void cancelDamage(LivingAttackEvent e) {
