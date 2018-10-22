b925880f87da39ac0694c41741868efca6650e11
LotuxPunk <clement@vandendaelen.com>
2018-10-23 00:10:52 +0200

    can't sleep without fix this

--- src/main/java/me/fril/angels/common/world/generation/WorldGenCatacombs.java
+++ src/main/java/me/fril/angels/common/world/generation/WorldGenCatacombs.java
@@ -3,6 +3,7 @@ package me.fril.angels.common.world.generation;
 import me.fril.angels.common.world.generation.generators.WorldGenStructure;
 import me.fril.angels.config.WAConfig;
 import me.fril.angels.WeepingAngels;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -16,6 +17,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -41,13 +43,14 @@ public class WorldGenCatacombs implements IWorldGenerator {
 	public static final WorldGenStructure CATACOMB_HALLWAY_BROKEN_2 = new WorldGenStructure(PATH_CATACOMB + "catacomb_hallway_broken_2");
 	public static final WorldGenStructure CATACOMB_HALLWAY_BROKEN_3 = new WorldGenStructure(PATH_CATACOMB + "catacomb_hallway_broken_3");
 
+	public static final List<String> structuresNames = Arrays.asList("catacomb_hallway_0", "catacomb_hallway_1", "catacomb_hallway_2", "catacomb_hallway_3", "catacomb_hallway_flat_1", "catacomb_hallway_flat_2", "catacomb_hallway_flat_3", "catacomb_hallway_clean_1", "catacomb_hallway_clean_2", "catacomb_hallway_clean_3", "catacomb_hallway_clean_4", "catacomb_hallway_broken_1", "catacomb_hallway_broken_2", "catacomb_hallway_broken_3");
 
-	public static final List<WorldGenStructure> CATACOMBS = Arrays.asList(CATACOMB_HALLWAY_0,CATACOMB_HALLWAY_1,CATACOMB_HALLWAY_2,CATACOMB_HALLWAY_3, CATACOMB_HALLWAY_FLAT_1, CATACOMB_HALLWAY_FLAT_2,CATACOMB_HALLWAY_FLAT_3,CATACOMB_HALLWAY_CLEAN_1,CATACOMB_HALLWAY_CLEAN_2,CATACOMB_HALLWAY_CLEAN_3,CATACOMB_HALLWAY_CLEAN_4,CATACOMB_HALLWAY_BROKEN_1,CATACOMB_HALLWAY_BROKEN_2,CATACOMB_HALLWAY_BROKEN_3);
+	//public static final List<WorldGenStructure> CATACOMBS = Arrays.asList(CATACOMB_HALLWAY_0,CATACOMB_HALLWAY_1,CATACOMB_HALLWAY_2,CATACOMB_HALLWAY_3, CATACOMB_HALLWAY_FLAT_1, CATACOMB_HALLWAY_FLAT_2,CATACOMB_HALLWAY_FLAT_3,CATACOMB_HALLWAY_CLEAN_1,CATACOMB_HALLWAY_CLEAN_2,CATACOMB_HALLWAY_CLEAN_3,CATACOMB_HALLWAY_CLEAN_4,CATACOMB_HALLWAY_BROKEN_1,CATACOMB_HALLWAY_BROKEN_2,CATACOMB_HALLWAY_BROKEN_3);
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		if (world.provider.getDimension() == 0){
-			WorldGenStructure theWinnerIs = CATACOMBS.get(random.nextInt(14));
+			WorldGenStructure theWinnerIs = new WorldGenStructure(PATH_CATACOMB + structuresNames.get(random.nextInt(structuresNames.size()-1)));
 			WeepingAngels.LOGGER.info(theWinnerIs.toString());
 			generateStructure(theWinnerIs,world,random,chunkX,chunkZ,4);
 		}
@@ -71,5 +74,9 @@ public class WorldGenCatacombs implements IWorldGenerator {
 	public static ResourceLocation getStraightPart(Random rand) {
 		return allStraightParts[rand.nextInt(allStraightParts.length)];
 	} */
+
+	private int getRandom(int lower, int higher){
+		return (int)(Math.random() * (higher-lower)) + lower;
+	}
 
 }
