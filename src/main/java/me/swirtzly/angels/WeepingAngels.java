dbc6abe67b712386295444e266df63853fa6d06d
Swirtzly <sandedshoes@gmail.com>
2020-03-20 12:42:34 +0000

    RB
    Signed-off-by: Swirtzly <sandedshoes@gmail.com>

--- src/main/java/me/swirtzly/angels/WeepingAngels.java
+++ src/main/java/me/swirtzly/angels/WeepingAngels.java
@@ -16,7 +16,7 @@ public class WeepingAngels {
 
     public static final String MODID = "weeping-angels";
     public static final String NAME = "Weeping Angels";
-    public static final String VERSION = "1.12.2-43";
+    public static final String VERSION = "1.12.2-44";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.2.2638,)";
     public static final String VERSION_CHECK = "https://raw.githubusercontent.com/Suffril/Weeping-Angels-Mod/master/update.json";
 
