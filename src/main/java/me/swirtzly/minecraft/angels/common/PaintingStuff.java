9d188e34ef43c63f560175054aab905219913b96
Craig <sandedshoes@gmail.com>
2021-02-07 23:27:07 +0000

    Started on catacombs Not going well

--- src/main/java/me/swirtzly/minecraft/angels/common/PaintingStuff.java
+++ src/main/java/me/swirtzly/minecraft/angels/common/PaintingStuff.java
@@ -10,6 +10,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class PaintingStuff {
     public static final DeferredRegister< PaintingType > PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, WeepingAngels.MODID);
 
-    public static final RegistryObject<PaintingType> HAVE_THE_PHONE_BOX = PAINTINGS.register("have_the_phone_box", () -> new PaintingType(64,56));
-    public static final RegistryObject<PaintingType> ANGEL = PAINTINGS.register("angel", () -> new PaintingType(64,56));
+    public static final RegistryObject< PaintingType > HAVE_THE_PHONE_BOX = PAINTINGS.register("have_the_phone_box", () -> new PaintingType(64, 56));
+    public static final RegistryObject< PaintingType > ANGEL = PAINTINGS.register("angel", () -> new PaintingType(64, 56));
+    public static final RegistryObject< PaintingType > ANGEL_NETHER = PAINTINGS.register("angel_nether", () -> new PaintingType(64, 56));
 }
