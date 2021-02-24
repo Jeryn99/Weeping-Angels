d01c62b1c60ebfc0b501361917f48b47364f3972
Craig <sandedshoes@gmail.com>
2021-02-24 11:36:22 +0000

    Model looks somewhat better

--- src/main/java/me/suff/mc/angels/client/models/WeepingAngelModel.java
+++ src/main/java/me/suff/mc/angels/client/models/WeepingAngelModel.java
@@ -3,14 +3,9 @@ package me.suff.mc.angels.client.models;
 import me.suff.mc.angels.common.entity.WeepingAngelEntity;
 import me.suff.mc.angels.enums.WeepingAngelPose;
 import net.minecraft.client.model.*;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.entity.PlayerModelPart;
-import net.minecraft.client.render.entity.model.*;
+import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Identifier;
-
-import java.util.function.Function;
 
 /* Created by Craig on 18/02/2021 */
 public class WeepingAngelModel extends EntityModel< WeepingAngelEntity > {
@@ -38,29 +33,26 @@ public class WeepingAngelModel extends EntityModel< WeepingAngelEntity > {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
 
-       // head = new ModelRenderer(this);
+        // head = new ModelRenderer(this);
 
-     String[] names = new String[]{"rightWing","leftWing", "left_arm", "right_arm", "body","legs", "head"};
+        String[] names = new String[]{"rightWing", "leftWing", "left_arm", "right_arm", "body", "legs", "head"};
         for (String name : names) {
             modelPartData.addChild(name, ModelPartBuilder.create().uv(24, 0).cuboid(-3.0F, -6.0F, -1.0F, 6.0F, 6.0F, 1.0F, Dilation.NONE), ModelTransform.NONE);
         }
-        ModelPartData headPart = modelPartData.addChild("head", ModelPartBuilder.create().uv(0, 17).cuboid(null, -4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, Dilation.NONE), ModelTransform.NONE);
-        headPart.addChild("hat", ModelPartBuilder.create().uv(72, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, Dilation.NONE.add(0.5F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-        modelPartData.addChild("left_arm", ModelPartBuilder.create().uv(24, 59).cuboid(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, Dilation.NONE), ModelTransform.pivot(5.0F, 2.5F, 0.0F));
-        modelPartData.addChild("right_arm", ModelPartBuilder.create().uv(10, 59).cuboid(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, Dilation.NONE), ModelTransform.pivot(-5.0F, 2.5F, 0.0F));
-        ModelPartData legs = modelPartData.addChild("legs", ModelPartBuilder.create().uv(40, 0).cuboid(-5, 4, -3, 10, 11, 6, Dilation.NONE), ModelTransform.pivot(-5.0F, 2.5F, 0.0F));
-        legs.addChild("legs_2", ModelPartBuilder.create().uv(0, 0).cuboid(-6, 0, -4, 12, 4, 8, Dilation.NONE), ModelTransform.pivot(-5.0F, 2.5F, 0.0F));
-        ModelPartData bodyPart = modelPartData.addChild("body", ModelPartBuilder.create().uv(56, 17).cuboid(-4, 12, -2.0F, 8,12,4, Dilation.NONE), ModelTransform.pivot(-5.0F, 2.5F, 0.0F));
-        bodyPart.addChild("outer_body", ModelPartBuilder.create().uv(56, 17).cuboid(-4, 12, -2.0F, 8,12,4, Dilation.NONE.add(0.5F)), ModelTransform.pivot(-5.0F, 2.5F, 0.0F));
-
-
-       /* String[] names = new String[]{"rightWing","leftWing", "left_arm", "right_arm", "body","legs", "head"};
+        ModelPartData headPart = modelPartData.addChild("head", ModelPartBuilder.create().uv(0, 17).cuboid(null, -4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, Dilation.NONE.add(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        headPart.addChild("head_2", ModelPartBuilder.create().uv(72, 0).cuboid(null, -4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, Dilation.NONE.add(0.5F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData bodyPart = modelPartData.addChild("body", ModelPartBuilder.create().uv(56, 17).cuboid(null, -4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, Dilation.NONE.add(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        bodyPart.addChild("body_2", ModelPartBuilder.create().uv(32, 17).cuboid(null, -4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, Dilation.NONE.add(0.5F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        modelPartData.addChild("left_arm", ModelPartBuilder.create().uv(24, 59).cuboid(null, -2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, Dilation.NONE.add(0.0F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+        modelPartData.addChild("right_arm", ModelPartBuilder.create().uv(10, 59).cuboid(null, -1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, Dilation.NONE.add(0.0F)), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+        ModelPartData legs = modelPartData.addChild("legs", ModelPartBuilder.create().uv(40, 0).cuboid(null, -5.0F, -0.25F, -3.0F, 10.0F, 11.0F, 6.0F, Dilation.NONE.add(0.0F)), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+        legs.addChild("legs_2", ModelPartBuilder.create().uv(0, 0).cuboid(null, -6.0F, 10.75F, -4.0F, 12.0F, 4.0F, 8.0F, Dilation.NONE.add(0.0F)), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
+        /* String[] names = new String[]{"rightWing","leftWing", "left_arm", "right_arm", "body","legs", "head"};
         for (String name : names) {
             modelPartData.addChild(name, ModelPartBuilder.create().uv(24, 0).cuboid(-3.0F, -6.0F, -1.0F, 6.0F, 6.0F, 1.0F, Dilation.NONE), ModelTransform.NONE);
         }*/
         return TexturedModelData.of(modelData, 128, 128);
     }
-
 
 
     public WeepingAngelPose getWeepingAngelPose() {
