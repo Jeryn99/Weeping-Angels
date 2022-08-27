849c9f3733db8f48c4473ae6367d8650d1e2eb71
Craig <sandedshoes@gmail.com>
2022-08-27 20:25:23 +0100

    Potential automation of fabric.mod.json & mod.toml

--- fabric/src/main/java/mc/craig/software/angels/fabric/WeepingAngelsFabric.java
+++ fabric/src/main/java/mc/craig/software/angels/fabric/WeepingAngelsFabric.java
@@ -1,11 +1,11 @@
 package mc.craig.software.angels.fabric;
 
-import net.examplemod.ExampleMod;
+import mc.craig.software.angels.WeepingAngels;
 import net.fabricmc.api.ModInitializer;
 
 public class WeepingAngelsFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        ExampleMod.init();
+        WeepingAngels.init();
     }
 }
--- forge/src/main/java/mc/craig/software/angels/forge/WeepingAngelsForge.java
+++ forge/src/main/java/mc/craig/software/angels/forge/WeepingAngelsForge.java
@@ -1,24 +1,58 @@
 package mc.craig.software.angels.forge;
 
-import dev.architectury.platform.forge.EventBuses;
+import mc.craig.software.angels.WAConfiguration;
 import mc.craig.software.angels.WeepingAngels;
+import mc.craig.software.angels.common.WAEntities;
+import mc.craig.software.angels.common.WASounds;
+import mc.craig.software.angels.common.blockentity.WABlockEntities;
+import mc.craig.software.angels.common.blocks.WABlocks;
+import mc.craig.software.angels.common.entity.angel.BlockReactions;
+import mc.craig.software.angels.common.entity.angel.WeepingAngel;
+import mc.craig.software.angels.common.items.WAItems;
+import mc.craig.software.angels.compat.vivecraft.ServerReflector;
 import mc.craig.software.angels.data.forge.*;
-import net.examplemod.ExampleMod;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.StartupMessageManager;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(WeepingAngels.MODID)
 public class WeepingAngelsForge {
 
-    public static final String MODID = "weeping_angels";
+    public static final ServerReflector VR_REFLECTOR = new ServerReflector();
+
 
     public WeepingAngelsForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         WeepingAngels.init();
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::onAttributeAssign);
+        modEventBus.addListener(this::onGatherData);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+        WAItems.ITEMS.register(modEventBus);
+        WASounds.SOUNDS.register(modEventBus);
+        WABlocks.BLOCKS.register(modEventBus);
+        WAEntities.ENTITY_TYPES.register(modEventBus);
+        WABlockEntities.BLOCK_ENTITY_TYPES.register(modEventBus);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, WAConfiguration.CONFIG_SPEC);
+        StartupMessageManager.addModMessage("Don't Blink!");
+
     }
 
     public void onGatherData(GatherDataEvent e) {
@@ -34,6 +68,18 @@ public class WeepingAngelsForge {
         generator.addProvider(true, new BlockTags(generator, existingFileHelper));
         generator.addProvider(true, new EntityTypeTags(generator, existingFileHelper));
         generator.addProvider(true, new ItemTags(generator, new BlockTags(generator, existingFileHelper), existingFileHelper));
+    }
+
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        BlockReactions.init();
+        SpawnPlacements.register(WAEntities.WEEPING_ANGEL.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Monster::checkMonsterSpawnRules);
+        VR_REFLECTOR.init();
+    }
+
+    public void onAttributeAssign(EntityAttributeCreationEvent event) {
+        event.put(WAEntities.WEEPING_ANGEL.get(), WeepingAngel.createAttributes().build());
+        event.put(WAEntities.ANOMALY.get(), WeepingAngel.createAttributes().build());
     }
 
 }
