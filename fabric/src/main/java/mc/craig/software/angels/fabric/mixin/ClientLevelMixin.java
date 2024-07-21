ffe19f962e91bbee6ee1b1177573ea3527313f04
Craig <sandedshoes@gmail.com>
2024-07-21 11:18:41 +0100

    Fixed Detector Sound

--- fabric/src/main/java/mc/craig/software/angels/fabric/mixin/ClientLevelMixin.java
+++ fabric/src/main/java/mc/craig/software/angels/fabric/mixin/ClientLevelMixin.java
@@ -2,7 +2,8 @@ package mc.craig.software.angels.fabric.mixin;
 
 import mc.craig.software.angels.util.ClientUtil;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientLevel.class)
 public class ClientLevelMixin {
 
-    //TODO
-    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/multiplayer/ClientLevel$EntityCallbacks;onTrackingStart(Lnet/minecraft/world/entity/Entity;)V")
-    private void addPlayer(int playerId, AbstractClientPlayer playerEntity, CallbackInfo ci) {
-        ClientUtil.playDectorSound(playerEntity);
-
+    @Inject(at = @At("HEAD"), method = "addEntity(Lnet/minecraft/world/entity/Entity;)V")
+    private void addPlayer(Entity entity, CallbackInfo ci) {
+        if (entity instanceof Player player) {
+            ClientUtil.playDectorSound(player);
+        }
     }
 
 
